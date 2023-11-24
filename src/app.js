4c8538e63642988d4d78f1ed442ea23e79e869e0
AlbertoRom <albertoromerocabezas@gmail.com>
2023-11-24 11:36:12 +0100

    loadCharacterPage function fixed

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 function loadCharacterPage(character){
     current_page = window.location.href
-    character_page = character + '.html'
+    character_page = character
     window.location.href = current_page.replace('reparto.html', character_page)
 }
